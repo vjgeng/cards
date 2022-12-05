24b8df1fd7904d61ee21f636b6752e216fece37e
vatchapol jittipattanakulchai <jitti.geng@gmail.com>
2022-12-05 14:49:42 +0700

    discovery pattern

--- src/main/java/com/eaztbytes/cards/controller/CardsController.java
+++ src/main/java/com/eaztbytes/cards/controller/CardsController.java
@@ -4,20 +4,30 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eaztbytes.cards.config.CardsServiceConfig;
 import com.eaztbytes.cards.model.Cards;
 import com.eaztbytes.cards.model.Customer;
+import com.eaztbytes.cards.model.Properties;
 import com.eaztbytes.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class CardsController {
 
 	@Autowired
 	private CardsRepository cardsRepository;
+	
 
-	@GetMapping("/myCards")
+	@Autowired
+	CardsServiceConfig cardsConfig;
+
+	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
 		if (cards != null) {
@@ -26,6 +36,15 @@ public class CardsController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+				cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
